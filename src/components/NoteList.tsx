119f1c45bb84553c4f295414e0cfd6e937e5d861
Volodymyr Lysenko <tibank2071@gmail.com>
2022-09-30 08:26:09 +0300

    feat: add buttons on modal form

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -8,57 +8,68 @@ import NoteForm from './NoteForm';
 import ModalNote from './ModalNote';
 
 const NoteList: FC = () => {
-  const notesService: NotesService = new NotesService(new DatastoreService());
-  const [notes, setNotes] = useState<Note[]>(notesService.findAll());
-  const [visible, setVisible] = useState<Boolean>(false);
+    const notesService: NotesService = new NotesService(new DatastoreService());
+    const [notes, setNotes] = useState<Note[]>(notesService.findAll());
+    const [visible, setVisible] = useState<Boolean>(false);
 
-  let emptyNote = {
-    id: 0,
-    name: 'New note',
-    created: new Date(),
-    category: { id: 1, name: 'Task' },
-    content: '',
-    archived: false,
-  };
-  const [selectedNote, setSelectedNote] = useState<Note>(emptyNote);
+    let emptyNote = {
+        id: 0,
+        name: 'New note',
+        created: new Date(),
+        category: { id: 1, name: 'Task' },
+        content: '',
+        archived: false,
+    };
+    const [selectedNote, setSelectedNote] = useState<Note>(emptyNote);
 
-  function updateNote(note: Note): void {
-    notesService.update(note.id, note);
-    setNotes([...notesService.findAll()]);
-  }
+    function updateNote(note: Note): void {
+        notesService.update(note.id, note);
+        setNotes([...notesService.findAll()]);
+    }
 
-  function archivedNote(note: Note): void {
-    note.archived = true;
-    updateNote(note);
-  }
+    function archivedNote(note: Note): void {
+        note.archived = true;
+        updateNote(note);
+    }
 
-  function removeNote(note: Note): void {
-    notesService.remove(note.id);
-    setNotes([...notesService.findAll()]);
-  }
+    function removeNote(note: Note): void {
+        notesService.remove(note.id);
+        setNotes([...notesService.findAll()]);
+    }
 
-  function editNote(note: Note): void {}
+    function editNote(note: Note): void {}
 
-  return (
-    <div className='list-notes__items'>
-      <HeaderList />
-      {notes.map(
-        (note: Note) =>
-          !note.archived && (
-            <NoteItem
-              note={note}
-              key={note.id}
-              editNote={editNote}
-              archivedNote={archivedNote}
-              removeNote={removeNote}
-            />
-          )
-      )}
-      <ModalNote visible={visible} setVisible={setVisible}>
-        <NoteForm title='New note' note={selectedNote} />
-      </ModalNote>
-    </div>
-  );
+    function createNote() {}
+
+    return (
+        <div className='list-notes__items'>
+            <HeaderList />
+            {notes.map(
+                (note: Note) =>
+                    !note.archived && (
+                        <NoteItem
+                            note={note}
+                            key={note.id}
+                            editNote={editNote}
+                            archivedNote={archivedNote}
+                            removeNote={removeNote}
+                        />
+                    )
+            )}
+            <div className='create-note__button'>
+                <button
+                    className='notes__btn'
+                    id='create-note__btn'
+                    onClick={() => setVisible(true)}
+                >
+                    Create note
+                </button>
+            </div>
+            <ModalNote visible={visible} setVisible={setVisible}>
+                <NoteForm note={selectedNote} />
+            </ModalNote>
+        </div>
+    );
 };
 
 export default NoteList;
